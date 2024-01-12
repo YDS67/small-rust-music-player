5feb3d79b1a4bc690a2145364309d935893e5aee
Yuriy D. Sibirmovsky <76599593+YDS67@users.noreply.github.com>
2024-01-12 17:36:59 +0300

    Small fix

--- src/audio.rs
+++ src/audio.rs
@@ -75,7 +75,7 @@ fn track_name(text: &String) -> String {
     let text1: Vec<&str> = text.split(|c| c == '/' || c == '\\').collect();
     let letters: Vec<char> = text1[text1.len()-1].chars().collect();
     let mut new: String = String::new();
-    for i in 0..(letters.len()).min(40) {
+    for i in 0..letters.len() {
         new.push(letters[i]);
     }
     let split: Vec<&str> = new.split(|c| c == '.').collect();
